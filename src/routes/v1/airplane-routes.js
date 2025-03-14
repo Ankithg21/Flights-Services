b724e016e39ca82a1a5caab4d41d833fd1ef039b
Ankithg21 <ankithg2104@gmail.com>
2025-03-14 17:48:29 +0530

    Implemented getAll api routes

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const {AirplaneMiddlewares} = require("../../middlewares/index.js");
 const {AirplaneController} = require("../../controllers/index.js");
 
+// /api/v1/airplanes POST
 router.post(
     "/",
     AirplaneMiddlewares.validateCreateRequest,
     AirplaneController.createAirplane
+);
+
+// /api/v1/airplanes GET
+router.get(
+    "/",
+    AirplaneController.getAirplanes
 );
 
 module.exports = router;
