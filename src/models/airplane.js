3c4650939986c37b29c8cf6d4d59600d53035232
Ankithg21 <ankithg2104@gmail.com>
2025-03-25 05:52:01 +0530

    Implemented Seat model and added inner joins for Flights by using include keyword

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -13,7 +13,11 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       this.hasMany(models.Flight,{
         foreignKey:'airplaneId',
-        onDelete:'CASCADE'
+        onDelete:'CASCADE',
+      });
+      this.hasMany(models.Seat,{
+        foreignKey:'airplaneId',
+        onDelete:'CASCADE',
       });
     }
   }
--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,6 +1,6 @@
 const CrudRepository = require("./crud-repository.js");
-const {Flight} = require("../models/index.js");
-const { where } = require("sequelize");
+const {Flight, Airplane, Airport, City} = require("../models/index.js");
+const { Sequelize, where } = require("sequelize");
 
 class FlightRepository extends CrudRepository{
     constructor(){
@@ -11,6 +11,37 @@ class FlightRepository extends CrudRepository{
         const response = await Flight.findAll({
             where:filter,
             order:sort,
+            include: [
+                {
+                    model: Airplane,
+                    required: true,
+                    as:'airplane_details'
+                },
+                {
+                    model:Airport,
+                    required:true,
+                    as:'departure_airport',
+                    on:{
+                        col1:Sequelize.where(Sequelize.col("Flight.departureAirportId"),"=",Sequelize.col("departure_airport.code")),                    
+                    },
+                    include:{
+                        model:City,
+                        required:true,
+                    }
+                },
+                {
+                    model:Airport,
+                    required:true,
+                    as:'arrival_airport',
+                    on:{
+                        col1:Sequelize.where(Sequelize.col("Flight.arrivalAirportId"),"=",Sequelize.col("arrival_airport.code")),                    
+                    },
+                    include:{
+                        model:City,
+                        required:true,
+                    }
+                }
+            ]  
         });
         return response;
     }
